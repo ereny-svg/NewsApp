c058810aa9c3c12ecdd90766a4bc757ceca0cdfa
ereny-svg <erenymahrous123@gmail.com>
2024-12-07 06:01:46 +0200

    finish details of news

---
+++ lib/news/news_details.dart
@@ -0,0 +1,136 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:newsapp/app_theme.dart';
+import 'package:newsapp/news/new_item.dart';
+import 'package:newsapp/widgets/loading_indicator.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:url_launcher/url_launcher.dart';
+
+class NewsDetails extends StatefulWidget {
+  static const String routename = '/details';
+
+  @override
+  State<NewsDetails> createState() => _NewsDetailsState();
+}
+
+class _NewsDetailsState extends State<NewsDetails> {
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    final news = ModalRoute.of(context)?.settings.arguments as NewItem;
+    return Container(
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          image:
+              DecorationImage(image: AssetImage('assets/images/pattern.png'))),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            news.news.source?.name ?? '',
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 29),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(5),
+                child: CachedNetworkImage(
+                  imageUrl: news.news.urlToImage ??
+                      'https://sesupport.edumall.jp/hc/article_attachments/900009570963/noImage.jpg',
+                  height: height * 0.25,
+                  width: double.infinity,
+                  fit: BoxFit.fill,
+                  placeholder: (context, url) => const LoadingIndicator(),
+                  errorWidget: (context, url, error) =>
+                      const Icon(Icons.image_not_supported_outlined),
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      news.news.source?.name ?? '',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleSmall!
+                          .copyWith(color: AppTheme.gray),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      news.news.title ?? "",
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                          color: AppTheme.darkgray,
+                          fontWeight: FontWeight.w500),
+                    ),
+                    Align(
+                      alignment: AlignmentDirectional.centerEnd,
+                      child: Text(
+                        timeago.format(news.news.publishedAt!),
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium!
+                            .copyWith(color: AppTheme.lightgray, fontSize: 13),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 18,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                          width: double.infinity,
+                          height: height * 0.370,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(25)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Container(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 11, vertical: 24),
+                                  child: Text(news.news.description ?? "")),
+                              const SizedBox(
+                                height: 55,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  launchUrl(Uri.parse(news.news.url ?? ''));
+                                  setState(() {});
+                                },
+                                child: Container(
+                                  margin: EdgeInsets.only(left: width * 0.64),
+                                  child: Text(
+                                    'View Full Article',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .titleMedium!
+                                        .copyWith(
+                                            color: AppTheme.darkgray,
+                                            fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/news/news_list.dart
+++ lib/news/news_list.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:newsapp/api/api_service.dart';
 import 'package:newsapp/news/new_item.dart';
+import 'package:newsapp/news/news_details.dart';
 import 'package:newsapp/widgets/error_indicator.dart';
 import 'package:newsapp/widgets/loading_indicator.dart';
 
 class NewsList extends StatelessWidget {
- NewsList({super.key,required this.sourceId});
-final String sourceId;
+  NewsList({super.key, required this.sourceId});
+  final String sourceId;
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(future: ApiService.getNews(sourceId), 
-    builder: (context, snapshot) {
-      if(snapshot.connectionState==ConnectionState.waiting){
-        return LoadingIndicator();
-      }
-      else if(snapshot.hasError || snapshot.data?.status!='ok'){
-        return ErrorIndicator();
-      }else{
-       final  news=snapshot.data?.articles??[];
-       return ListView.builder(itemBuilder: (context, index) => NewItem(news: news[index]),itemCount: news.length,);
-      }
-    },);
+    return FutureBuilder(
+      future: ApiService.getNews(sourceId),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return LoadingIndicator();
+        } else if (snapshot.hasError || snapshot.data?.status != 'ok') {
+          return ErrorIndicator();
+        } else {
+          final news = snapshot.data?.articles ?? [];
+          return ListView.builder(
+            itemBuilder: (context, index) => GestureDetector(
+                onTap: () =>
+                    Navigator.of(context).pushNamed(NewsDetails.routename,arguments: NewItem(news: news[index]) ),
+                child: NewItem(news: news[index])),
+            itemCount: news.length,
+          );
+        }
+      },
+    );
   }
 }
